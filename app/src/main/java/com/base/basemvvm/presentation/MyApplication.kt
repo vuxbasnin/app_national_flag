b7270fd40cec7ffb8753cbb3d6438a81932c7b28
Nin <ninvuba@vccorp.vn>
2024-04-11 16:28:32 +0700

    [Base] config timber log

--- app/src/main/java/com/base/basemvvm/presentation/MyApplication.kt
+++ app/src/main/java/com/base/basemvvm/presentation/MyApplication.kt
@@ -1,12 +1,18 @@
 package com.base.basemvvm.presentation
 
 import android.app.Application
+import com.base.basemvvm.BuildConfig
+import com.base.basemvvm.core.utils.LogsUtil
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class MyApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         //config, init sdk in layer application
+        if (BuildConfig.DEBUG) {
+            Timber.plant(LogsUtil())
+        }
     }
 }
