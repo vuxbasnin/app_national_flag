7fbf6240f4cff4489af70e4f475757bcf6937ea2
Nin <ninvuba@vccorp.vn>
2024-04-12 17:28:44 +0700

    [App] custom view semi cycle

--- app/src/main/java/com/base/basemvvm/presentation/feature/m02/M02Fragment.kt
+++ app/src/main/java/com/base/basemvvm/presentation/feature/m02/M02Fragment.kt
@@ -1,13 +1,21 @@
 package com.base.basemvvm.presentation.feature.m02
 
 import android.view.View
+import com.base.basemvvm.R
 import com.base.basemvvm.databinding.M02FragmentBinding
 import com.base.basemvvm.presentation.core.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.random.Random
 
 @AndroidEntryPoint
 class M02Fragment: BaseFragment<M02FragmentBinding>(M02FragmentBinding::inflate) {
     override fun initView() {
+        binding.semiCycle.setUpUI(R.color.primary)
+        binding.btnChangeProgress.setOnClickListener {
+            val progress = Random.nextInt(0, 100)
+            binding.tvProgress.text = "$progress %"
+            binding.semiCycle.setProcess(progress.toFloat()/100f)
+        }
     }
 
     override fun initObserver() {
